dfa66b37b9c342e27bc2071ce4c042039240f491
leigh schrandt <leigh@null.net>
2017-05-30 09:23:16 -0600

    added fatch loops for nodes and tasks + updated service rendering

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -3,27 +3,33 @@
     a-sky(
       color='midnightblue'
     )
-    a-circle(
+    a-plane(
       position='0 0 -4'
       rotation='-90 0 0'
       color='grey'
-      radius=30
+      width=10
+      height=10
     )
-    a-box(
+    a-entity(
       v-for='(service, index) in services'
-      :key='service.ID'
-      :position='(index+0.5-services.length/2)*2 + " 2 -4"'
-      rotation='0 45 0'
-      color='blue'
+      :position='(index+0.5-services.length/2)*2 + " .1 -4"'
     )
-    a-text(
-      v-for='(service, index) in services'
-      :key='service.ID'
-      :value='service.Spec.Name'
-      :position='(index+0.1-services.length/2)*2 + " 2 -4"'
-      rotation='0 -45 0'
-      color='white'
-    )
+      a-plane(
+        position='0 0 0'
+        rotation='-90 0 0'
+      )
+      a-entity(position='0 1 0')
+        a-box(
+          rotation='0 45 0'
+          color='indigo'
+        )
+        a-text(
+          position='-.8 0 0'
+          :value='service.Spec.Name'
+          rotation='0 -45 0'
+          color='yellow'
+          side='double'
+        )
 </template>
 
 <script>
@@ -31,24 +37,56 @@
 
   export default {
     data: () => ({
+      nodes: [],
       services: [],
-      serviceFetcher: null
+      tasks: [],
+      nodeFetcher: null,
+      serviceFetcher: null,
+      taskFetcher: null,
     }),
+    computed: {
+      vrNodes () {
+        return nodes.map( node => ({}))
+      }
+    },
     methods: {
+      getNodes() {
+        return fetch( 'docker/nodes' )
+        .then( response => response.json() )
+        .then( nodes =>
+          this.nodes = nodes.sort(
+            (a,b) =>  a.Spec.Name < b.Spec.Name
+          )
+        )
+      },
       getServices() {
-        fetch( 'docker/services' )
+        return fetch( 'docker/services' )
         .then( response => response.json() )
         .then( services =>
           this.services = services.sort(
             (a,b) =>  a.Spec.Name < b.Spec.Name
           )
         )
-      }
+      },
+      getTasks() {
+        return fetch( 'docker/tasks' )
+        .then( response => response.json() )
+        .then( tasks =>
+          this.tasks = tasks.sort(
+            (a,b) =>  a.ServiceID < b.ServiceID
+          )
+        )
+      },
     },
     created () {
-      this.getServices()
+      let getAll = () => Promise.All([
+        this.getNodes(),
+        this.getServices(),
+        this.getTasks(),
+      ])
+      getAll()
       this.serviceFetcher = setInterval(
-        this.getServices, 1000
+        getAll, 1000
       )
     },
     destroy () {
