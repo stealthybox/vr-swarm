ee6d738ee5bdc13cbd5545e458fcd98ec175103f
leigh schrandt <leigh@null.net>
2017-10-17 11:50:49 +0200

    Add log prototype

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -1,5 +1,30 @@
 <template lang='pug'>
   a-scene
+    a-camera
+      a-entity(
+        cursor='fuse: true;'
+        raycaster='far: 20; objects: .clickable'
+        position='0 0 -1'
+        geometry='primitive: ring; radiusInner: 0.02; radiusOuter: 0.03'
+        material='color: white; shader: flat'
+      )
+        a-animation(
+          begin='click'
+          easing='ease-in'
+          attribute='scale'
+          dur='150'
+          fill='forwards'
+          from='0.1 0.1 0.1'
+          to='1 1 1'
+        )
+        a-animation(
+          begin='cursor-fusing'
+          easing='ease-in'
+          attribute='scale'
+          dur='1500'
+          fill='backwards'
+          from='1 1 1' to='0.1 0.1 0.1'
+        )
     a-sky(
       color='midnightblue'
     )
@@ -24,13 +49,16 @@
         rotation='-90 0 0'
         color='black'
         side='double'
-      )
-      a-entity(
-        v-for='(task, index) in tasks.filter(t => t.DesiredState !== "shutdown" && t.NodeID === node.ID)'
-        :position='"0 " + (index*1.5 + 1) + " 0"'
+        )
+      a-entity.clickable(
+      v-for='(task, index) in tasks.filter(t => t.DesiredState !== "shutdown" && t.NodeID === node.ID)'
+      :key='task.ID'
+      :position='`0 ${index*1.5 + 1.6} 0`'
+      @click='setSelectedService(task.ServiceID, task.ID)'
       )
         a-box(
-          color='indigo'
+        color='indigo'
+        @mouseenter='setServiceColor(task.ServiceID ,"#444444", $event)'
         )
         a-text(
           :value='services.find( s => s.ID === task.ServiceID ).Spec.Name'
@@ -38,6 +66,18 @@
           color='yellow'
           side='double'
         )
+    a-box(
+    rotation='0 0 0'
+    position='-5 5.5 -10'
+    color='#333'
+    width=5
+    height=10
+    )
+      a-text(
+      :value='log'
+      position='-2 4.5 0.5'
+      side='double'
+      )
 </template>
 
 <script>
@@ -51,10 +91,16 @@
       nodeFetcher: null,
       serviceFetcher: null,
       taskFetcher: null,
+      decoder: new TextDecoder(),
+      log: 'test',
+      logReader: null,
+      selectedService: null,
+      serviceColors: {},
+      selectedNetwork: null,
     }),
     computed: {
       vrNodes () {
-        return nodes.map( node => ({}))
+        return this.nodes.map( node => ({}) )
       }
     },
     methods: {
@@ -84,6 +130,83 @@
             (a,b) =>  a.ServiceID < b.ServiceID
           )
         )
+      },
+      getLogs(serviceId) {
+        // this.log = 'reset'
+        return fetch(`/docker/services/${serviceId}/logs?stdout=true&stderr=true&follow=true`)
+        .then( response => {
+          const reader = response.body.getReader()
+          const decoder = new TextDecoder()
+          // const setLog = val => { this.logs = val }
+          const processLogs = (stream) => {
+            if (stream.done) {
+              return this.log
+            } else {
+              let text = decoder.decode(stream.value, {stream:true})
+              let lines = text
+                .split('\n')
+                .map( s => s.substring(38) )
+                .join('\n')
+              console.log('hi')
+              this.log = lines
+              return reader.read()
+              .then( processLogs )
+            }
+          }
+          return reader.read()
+          .then( processLogs )
+        })
+      },
+      fetchLogStream(serviceId) {
+        // this.log = 'reset'
+        fetch(`/docker/services/${serviceId}/logs?stdout=true&stderr=true&follow=true`)
+        .then( response => {
+          this.logReader = response.body.getReader()
+          // const setLog = val => { this.logs = val }
+          console.log('START READER', this.logReader)
+          return this.logReader.read()
+        })
+        .then( this.processLogs )
+      },
+      processLogs(logData) {
+        console.log('call', logData)
+        if (logData.done) {
+          console.log('END', logData)
+          return this.log
+        } else {
+          console.log('logDataValue', logData)
+          let text = this.decoder.decode(logData.value, {stream:true})
+          let lines = text
+            .split('\n')
+            .map( s => s.substring(38) )
+            .join('\n')
+          this.log += '\n'+ lines
+          return this.logReader.read()
+          .then( this.processLogs )
+        }
+      },
+      setSelectedService(serviceId, taskId) {
+        console.log('set service:', serviceId, 'from task:', taskId )
+        this.selectedService = serviceId
+        console.log('BEFORE', this.logReader)
+        this.logReader && this.logReader.cancel()
+        console.log('CANCEL', this.logReader)
+        this.fetchLogStream( serviceId )
+        console.log('AFTER', this.logReader)
+        return serviceId
+      },
+      setSelectedNetwork(networkId) {
+        console.log('set network:', networkId)
+        this.selectedNetwork = networkId
+        return networkId
+      },
+      setServiceColor(serviceId, color, e) {
+        this.serviceColors[serviceId] = color
+        console.log(serviceId, color, e.target)
+      },
+      getColor(serviceId) {
+        console.log(serviceId, this.serviceColors[serviceId])
+        return `hsl(197, 100%, ${this.selectedService === serviceId ? 20:60}%)`
       },
     },
     created () {
